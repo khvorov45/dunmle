073dd0e0e89ee1b31938ee444008a26d12c4c0fb
Sen Khvorov <khvorov45@gmail.com>
2019-07-26 22:09:41 +1000

    Added documentation and project files

---
+++ R/estimation.R
@@ -0,0 +1,9 @@
+#' Temporary funciton for testing purposes
+#'
+#' @param x Anything that can be passed to cat. New bit.
+#' @export
+#' @examples
+#' testing_documentation("test")
+testing_documentation <- function(x) {
+    cat("You passed", x, "\n")
+}
---
+++ devtesting/devtesting.R
@@ -0,0 +1,7 @@
+devtools::install_github("khvorov45/dunmle")
+
+devtools::install("G:/Nexus/dunmle")
+
+library(dunmle)
+
+testing_documentation("test")
