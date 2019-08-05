61399554a33340ebabbaba963fafa91ff8310b97
Khvorov <khvoroa@ssg.org.au>
2019-08-05 12:09:26 +1000

    debug evironment 1

--- R/fitting.R
+++ R/fitting.R
@@ -17,18 +17,19 @@
 sclr <- function(
   formula, data, tol = 10^(-7), n_iter = NULL, max_tol_it = 10^4
 ) {
-  
+  cat("very beginning\n")
   cl <- match.call()
   
   # Manipulations to get to response vector and design matrix
   
   ## The call is passed to model.frame
-  mf <- match.call()
+  mf <- match.call(expand.dots = FALSE)
   m <- match(c("formula", "data"), names(mf), 0L)
   mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
   mf[[1L]] <- quote(stats::model.frame)
-  mf <- eval(mf)
-  
+  mf <- eval(mf, parent.frame())
+  cat("just evaluated\n")
   ## Design matirix
   mt <- attr(mf, "terms")
   x <- model.matrix(mt, mf)
